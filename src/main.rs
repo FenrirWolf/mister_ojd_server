ab15c84c33163a72e45685da5a564b95892e1a70
Fenrir <fenrirwolf@gmail.com>
2021-05-05 21:40:07 -0600

    Refactor code and handle disconnects

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use inotify::{Inotify, EventMask, WatchMask};
 use nix::{ioctl_read, ioctl_read_buf};
 use serde_json::{json, Value};
@@ -15,6 +15,7 @@ struct Gamepad {
     name: String,
     buttons: Vec<bool>,
     axes: Vec<f64>,
+    connected: bool,
 }
 
 #[repr(C)]
@@ -36,134 +37,184 @@ ioctl_read!(get_num_axes, JSIOCG_MAGIC, JSIOCGAXES, u8);
 ioctl_read!(get_num_buttons, JSIOCG_MAGIC, JSIOCGBUTTONS, u8);
 ioctl_read_buf!(get_gamepad_name, JSIOCG_MAGIC, JSIOCGNAME, u8);
 
-fn main() -> Result<()> {
-    let mut stream = connect_to_ojd()?;
-    let mut gamepad = get_gamepad_info()?;
-    let mut recv_buf = [0u8; 26];
-
+fn main() {
     loop {
-        if stream.read(&mut recv_buf)? > 0 {
-            read_gamepad_events(&mut gamepad)?;
-            let payload = build_json_payload(&mut gamepad);
-            write!(&mut stream, "{}#{}", payload.len(), payload)?;
-        } else {
-            println!("No bytes from the server! Quitting...");
-            break;
+        println!("Waiting for Open Joystick Display");
+        let mut socket = match connect_to_ojd() {
+            Ok(stream) => {
+                println!("Connected to Open Joystick Display");
+                stream
+            },
+            Err(e) => {
+                println!("{}", e);
+                continue
+            }
+        };
+
+        let mut gamepad = Gamepad::new();
+        let mut recv_buf = [0u8; 26];
+
+        while let Ok(_) = socket.read(&mut recv_buf) {
+            if !gamepad.is_connected() {
+                if let Err(e) = gamepad.connect() {
+                    println!("{}", e);
+                } else {
+                    println!("Gamepad connected");
+                }
+            }
+
+            if let Err(_) = gamepad.update_state() {
+                println!("Gamepad disconnected");
+            }
+
+            let payload = gamepad.build_json_payload();
+            if let Err(e) = write!(&mut socket, "{}#{}", payload.len(), payload) {
+                println!("{}", e);
+            }
         }
+
+        println!("Disconnected from Open Joystick Display");
     }
 
-    Ok(())
 }
 
 fn connect_to_ojd() -> Result<TcpStream> {
     let mut ip_addr = local_ipaddress::get().context("Unable to retrieve local IP address")?;
     ip_addr.push_str(":56709");
     
-    println!("binding to {}...", ip_addr);
     let listener = TcpListener::bind(ip_addr).context("Unable to bind to local IP address")?;
 
-    println!("accepting...");
     let (stream, _) = listener.accept().context("Unable to accept TCP connection")?;
 
-    println!("Connected!");
     Ok(stream)
 }
 
-fn get_gamepad_info() -> Result<Gamepad> {
-    wait_for_gamepad_connection()?;
-
-    let mut num_axes: u8 = 0;
-    let mut num_buttons: u8 = 0;
-    let mut gamepad_name = [0u8; 128];
-
-    let file = File::open("/dev/input/js0").context("No gamepad detected")?;
-    let fd = file.as_raw_fd();
-
-    unsafe {
-        get_num_axes(fd, &mut num_axes).context("Couldn't get gamepad axes")?;
-        get_num_buttons(fd, &mut num_buttons).context("Couldn't get gamepad buttons")?;
-        get_gamepad_name(fd, &mut gamepad_name).context("Couldn't get gamepad name")?;
+impl Gamepad {
+    fn new() -> Self {
+        Self {
+            name: String::from(""),
+            buttons: vec![],
+            axes: vec![],
+            connected: false,
+        }
     }
 
-    let name_len = gamepad_name.iter()
-        .position(|&ch| ch == b'\0')
-        .context("Gamepad name isn't nul-terminated somehow???")? + 1;
+    fn connect(&mut self) -> Result<()> {
+        self.wait_for_connection()?;
 
-    let parsed_name: String = CStr::from_bytes_with_nul(&gamepad_name[..name_len])
-        .context("Failed to parse gamepad name")?
-        .to_string_lossy()
-        .into();
-    
-    Ok(Gamepad {
-        name: parsed_name,
-        buttons: vec![false; num_buttons as usize],
-        axes: vec![0f64; num_axes as usize],
-    })
-}
+        let mut num_axes: u8 = 0;
+        let mut num_buttons: u8 = 0;
+        let mut gamepad_name = [0u8; 128];
 
-fn wait_for_gamepad_connection() -> Result<()> {
-    // skip the wait if `js0` is already connected
-    if let Ok(_) = File::open("/dev/input/js0") {
-        return Ok(());
+        let device = File::open("/dev/input/js0").context("No gamepad detected")?;
+        let fd = device.as_raw_fd();
+
+        unsafe {
+            get_num_axes(fd, &mut num_axes).context("Couldn't get gamepad axes")?;
+            get_num_buttons(fd, &mut num_buttons).context("Couldn't get gamepad buttons")?;
+            get_gamepad_name(fd, &mut gamepad_name).context("Couldn't get gamepad name")?;
+        }
+
+        let name_len = gamepad_name.iter()
+            .position(|&ch| ch == b'\0')
+            .context("Gamepad name isn't nul-terminated somehow???")? + 1;
+
+        let parsed_name: String = CStr::from_bytes_with_nul(&gamepad_name[..name_len])
+            .context("Failed to parse gamepad name")?
+            .to_string_lossy()
+            .into();
+        
+        self.name = parsed_name;
+        self.buttons.resize(num_buttons as usize, false);
+        self.axes.resize(num_axes as usize, 0 as f64);
+        self.connected = true;
+
+        Ok(())
     }
 
-    let mut inotify = Inotify::init()?;
-    inotify.add_watch("/dev/input", WatchMask::CREATE)?;
+    fn is_connected(&self) -> bool {
+        self.connected
+    }
 
-    let mut buf = [0; 1024];
-    loop {
-        let mut events = inotify.read_events_blocking(&mut buf)?;
+    fn wait_for_connection(&mut self) -> Result<()> {
+        // skip the wait if `js0` is already connected
+        if let Ok(_) = File::open("/dev/input/js0") {
+            return Ok(());
+        }
 
-        if let Some(event) = events.next() {
-            if let Some(name) = event.name {
-                if name == "js0" && event.mask == EventMask::CREATE {
-                    break
+        let mut inotify = Inotify::init()?;
+        inotify.add_watch("/dev/input", WatchMask::CREATE)?;
+
+        let mut buf = [0; 1024];
+        loop {
+            let mut events = inotify.read_events_blocking(&mut buf)?;
+
+            if let Some(event) = events.next() {
+                if let Some(name) = event.name {
+                    if name == "js0" && event.mask == EventMask::CREATE {
+                        break
+                    }
                 }
             }
         }
+
+        Ok(())
     }
 
-    Ok(())
-}
+    fn update_state(&mut self) -> Result<()> {
+        let mut device = match File::open("/dev/input/js0") {
+            Ok(device) => device,
+            Err(e) => {
+                self.connected = false;
+                return Err(anyhow!(e));
+            }
 
+        };
 
-fn read_gamepad_events(gamepad: &mut Gamepad) -> Result<()> {
-    let mut file = File::open("/dev/input/js0").context("No gamepad detected")?;
-    let mut raw = [0u8; 8];
+        let mut raw = [0u8; 8];
 
-    for button in &mut gamepad.buttons {
-        file.read(&mut raw).context("Couldn't read js_event")?;
-        let event: js_event = unsafe { std::mem::transmute(raw) };
-        *button = event.value != 0;
-    }
+        for button in &mut self.buttons {
+            if let Err(e) = device.read(&mut raw) {
+                self.connected = false;
+                return Err(anyhow!(e))
 
-    for axis in &mut gamepad.axes {
-        file.read(&mut raw).context("Couldn't read js_event")?;
-        let event: js_event = unsafe { std::mem::transmute(raw) };
-        *axis = event.value as f64 / i16::MAX as f64;
-    }
-
-    Ok(())
-}
-
-fn build_json_payload(gamepad: &Gamepad) -> String {
-    let buttons_json: Vec<Value> = gamepad.buttons.iter()
-        .map(|&val| {
-            json!({"pressed": val, "value": if val {1} else {0}})
-        })
-        .collect();
-
-    let payload = json!(
-        {
-            "axes": gamepad.axes,
-            "buttons": buttons_json,
-            "connected": true,
-            "id": gamepad.name,
-            "index": 0,
-            "mapping": "",
-            "timestamp": 0,
+            }
+            let event: js_event = unsafe { std::mem::transmute(raw) };
+            *button = event.value != 0;
         }
-    );
 
-    payload.to_string()
+        for axis in &mut self.axes {
+            if let Err(e) = device.read(&mut raw) {
+                self.connected = false;
+                return Err(anyhow!(e))
+
+            }
+            let event: js_event = unsafe { std::mem::transmute(raw) };
+            *axis = event.value as f64 / i16::MAX as f64;
+        }
+
+        Ok(())
+    }
+
+    fn build_json_payload(&mut self) -> String {
+        let buttons_json: Vec<Value> = self.buttons.iter()
+            .map(|&val| {
+                json!({"pressed": val, "value": if val {1} else {0}})
+            })
+            .collect();
+
+        let payload = json!(
+            {
+                "axes": self.axes,
+                "buttons": buttons_json,
+                "connected": true,
+                "id": self.name,
+                "index": 0,
+                "mapping": "",
+                "timestamp": 0,
+            }
+        );
+
+        payload.to_string()
+    }
 }
